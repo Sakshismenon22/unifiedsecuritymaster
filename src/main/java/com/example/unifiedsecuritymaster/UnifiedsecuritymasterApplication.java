41c87bea48502d56473636936fce609c6a5b94ab
Niranjan V <rathinamniranjan@gmail.com>
2026-09-24 11:34:21 +0530

    initial commit

---
+++ src/main/java/com/example/unifiedsecuritymaster/UnifiedsecuritymasterApplication.java
@@ -0,0 +1,13 @@
+package com.example.unifiedsecuritymaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnifiedsecuritymasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnifiedsecuritymasterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/unifiedsecuritymaster/UnifiedsecuritymasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.unifiedsecuritymaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnifiedsecuritymasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
