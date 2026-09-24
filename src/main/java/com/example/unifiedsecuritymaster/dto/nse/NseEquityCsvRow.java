657be25c4e40ae3447e1f52e9a675d0d02a97bac
Niranjan V <rathinamniranjan@gmail.com>
2026-09-24 14:48:03 +0530

    save

--- src/main/java/com/example/unifiedsecuritymaster/dto/nse/NseEquityCsvRow.java
+++ src/main/java/com/example/unifiedsecuritymaster/dto/nse/NseEquityCsvRow.java
@@ -1,4 +1,26 @@
-package com.example.unifiedsecuritymaster.dto;
+package com.example.unifiedsecuritymaster.dto.nse;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class NseEquityCsvRow {
+    private String symbol;
+    private String series;
+    private String tradeDate;
+    private String prevClose;
+    private String openPrice;
+    private String highPrice;
+    private String lowPrice;
+    private String lastPrice;
+    private String closePrice;
+    private String averagePrice;
+    private String totalTradedQuantity;
+    private String turnover;
+    private String noOfTrades;
+    private String deliverableQty;
+    private String deliveryPercentage;
 }
