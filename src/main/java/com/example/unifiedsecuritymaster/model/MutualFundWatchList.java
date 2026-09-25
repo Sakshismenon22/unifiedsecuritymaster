1826021e2d4b6c21933fd3a7b104a2594f2a7463
Sakshi Menon <2000209444@hexaware.com>
2026-09-25 11:15:48 +0530

    mutual funds dto and model

---
+++ src/main/java/com/example/unifiedsecuritymaster/model/MutualFundWatchList.java
@@ -0,0 +1,27 @@
+package com.example.unifiedsecuritymaster.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "mutualfund_watchlist")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class MutualFundWatchList {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String isin;
+
+    private String schemeName;
+
+    private Asset asset;
+
+}
